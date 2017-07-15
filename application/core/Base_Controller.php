c2d75de42d83d3423ed5ce1c597b1383f595f490
mahigupta <mahesh.gupta@hotmail.com>
2017-07-15 12:12:49 -0700

    Added comment in base controller

--- application/core/Base_Controller.php
+++ application/core/Base_Controller.php
@@ -10,6 +10,8 @@ class Base {
 		$this->database = new Database();
 		$this->session = new Session();
 		
+		// if user currently logged in, then update session cookie time to keep their session active
+		// reaching this function means, user interacted with UI.
 		if (_currently_logged_in()) {
 			$this->session->cookie(SESSION_USERNAME_COOKIE, $this->session->getCookie(SESSION_USERNAME_COOKIE), time() + SESSION_TIMEOUT);
 		}
